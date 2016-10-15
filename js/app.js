cd96b6b04f7800c4b6f966d914d750beb5199e86
abel abadi <abelabadi2@gmail.com>
2016-10-15 08:55:03 -0500

    consume openweather api
    make request to openweather api, templrature converter filter and
    display to brower

--- js/app.js
+++ js/app.js
@@ -18,7 +18,20 @@ weatherApp.controller('homeController',['$scope','cityService',function ($scope,
 		cityService.city = $scope.city;
 	});
 }]);
-weatherApp.controller('forcastController',['$scope','cityService',function ($scope,cityService) {
-	$scope.city = cityService.city;
+weatherApp.controller('forcastController',['$scope','$resource','cityService',
+	function ($scope,$resource,cityService) {
+		$scope.city = cityService.city;
+		$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
+			{callback:"JSON_CALLBACK"},
+			{get:{method:"JSONP"}});
+		$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city,
+			cnt:2,
+			appid:'2647b80ab9f831c4c797795d9817a89a'});
 
-}]);
+	}]);
+
+weatherApp.filter('fahrenheit',function () {
+	return function (degk) {
+		return Math.round((1.8*(degk-273))+32) +' F';
+	}
+});
