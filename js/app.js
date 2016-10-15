515c336456b677289d30cdc888f0c4a4453f18ae
abel abadi <abelabadi2@gmail.com>
2016-10-14 23:37:35 -0500

    set watcher for city
    created custom service for passing city information between controller

--- js/app.js
+++ js/app.js
@@ -9,10 +9,16 @@ weatherApp.config(function ($routeProvider) {
 		controller:'forcastController'
 	});
 });
-
-weatherApp.controller('homeController',['$scope',function ($scope) {
-	
+weatherApp.service('cityService',function () {
+	this.city = 'Fairfield, IA';
+});
+weatherApp.controller('homeController',['$scope','cityService',function ($scope,cityService) {
+	$scope.city = cityService.city;
+	$scope.$watch('city',function () {
+		cityService.city = $scope.city;
+	});
 }]);
-weatherApp.controller('forcastController',['$scope',function ($scope) {
-	
+weatherApp.controller('forcastController',['$scope','cityService',function ($scope,cityService) {
+	$scope.city = cityService.city;
+
 }]);
